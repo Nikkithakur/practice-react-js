a074ddb0dfc0e09dab7717c6d1a9f8816142e2e0
Prasad <thaprasad@deloitte.com>
2020-11-13 18:44:55 +0530

    2. export with and without default component

--- helloworld/src/App.js
+++ helloworld/src/App.js
@@ -1,7 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Greet from './components/Greet';
+//import Greet from './components/Greet';
+import {Greet} from './components/Greet';
 
+/**
+ * @author Nikhilprasad
+ * when not using default export functionality, use below changes
+ * //import Greet from './components/Greet'; -> import {Greet} from './components/Greet';
+ */
 function App() {
   return (
     <div className="App">
--- helloworld/src/components/Greet.js
+++ helloworld/src/components/Greet.js
@@ -1,15 +1,17 @@
 import React from 'react'
 
 /**
+ * @author NikhilPrasad
  * stateless functional component
  * 
  * function Greet(){
  *  return <h1>Hello Batman!!</h1>
  * }
- * 
  * can also write lambdas
+ * 
+ * - can also export the function directly
  */
 
-const Greet = () =>  <h1>Hello Batman!!</h1>
+export const Greet = () =>  <h1>Hello Batman!!</h1>
 
-export default Greet
+//export default Greet
